a02c7657c5e6c36a7c2910b8917238dbc9e752b4
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-08-10 19:29:52 +0300

    complete header & main banner

---
+++ app/js/script.js
@@ -0,0 +1,9 @@
+(function ($) {
+    "use strict";
+
+    // Page loading animation
+
+    $(window).on('load', function () {
+        $('#js-preloader').addClass('loaded')
+    });
+})(window.jQuery);
---
+++ dist/js/all.js
@@ -0,0 +1,2 @@
+!function(d){"use strict";d(window).on("load",(function(){d("#js-preloader").addClass("loaded")}))}(window.jQuery);
+//# sourceMappingURL=all.js.map
