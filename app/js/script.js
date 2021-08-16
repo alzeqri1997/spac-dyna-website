882b3da6aa723c8786cc89827a0e26b510a9d127
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-08-16 14:39:12 +0300

    add some functionality

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,31 @@
 (function ($) {
     "use strict";
 
+    // Header Type = Fixed
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        var header = $('header').height();
+
+        if (scroll >= header) {
+            $("header").addClass("background-header");
+        } else {
+            $("header").removeClass("background-header");
+        }
+    })
+
+    // Menu Dropdown Toggle 
+    if ($('.menu-trigger').length) {
+        $(".menu-trigger").on("click", function () {
+            $(this).toggleClass('active');
+            $(".header-area .nav").slideToggle();
+        });
+    }
+
+
+
+
+
+
     // Page loading animation
 
     $(window).on('load', function () {
